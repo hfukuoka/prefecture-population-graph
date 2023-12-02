ef226e818fd0b7a59c3621cfbfbc4bf171db9b09
hfukuoka <hayato.fukuoka.q2@gmail.com>
2023-12-02 17:54:07 +0900

    test: change test style

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -4,12 +4,16 @@ import App from "../App";
 // App全体をレンダリングして結合テスト
 // 実際のAPIを用いる
 describe("integration test", () => {
-  test("render", async () => {
+  beforeEach(() => {
     render(<App />);
+  });
 
+  test("title", async () => {
     const linkElement = screen.getByText("都道府県人口グラフ");
     expect(linkElement).toBeInTheDocument(); // タイトルが表示されているか
+  });
 
+  test("get prefecture", async () => {
     await screen.findAllByRole("checkbox");
     const checkboxs = screen.getAllByRole("checkbox");
     expect(checkboxs.length).toBe(47); // 47都道府県あるか
