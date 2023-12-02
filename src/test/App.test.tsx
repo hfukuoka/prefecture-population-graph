14447743fa0fcf403f1a59065c1804f90e13f0fd
hfukuoka <hayato.fukuoka.q2@gmail.com>
2023-12-02 17:48:00 +0900

    test: prefecture checkbox

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,17 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("都道府県人口グラフ");
-  expect(linkElement).toBeInTheDocument();
+// App全体をレンダリングして結合テスト
+// 実際のAPIを用いる
+describe("integration test", () => {
+  test("render", async () => {
+    render(<App />);
+
+    const linkElement = screen.getByText("都道府県人口グラフ");
+    expect(linkElement).toBeInTheDocument(); // タイトルが表示されているか
+
+    await screen.findAllByRole("checkbox");
+    const checkboxs = screen.getAllByRole("checkbox");
+    expect(checkboxs.length).toBe(47); // 47都道府県あるか
+  });
 });
