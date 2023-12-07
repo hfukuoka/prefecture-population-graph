d4c657f68bdb09f5c87f37efcf371af81a004154
hfukuoka <hayato.fukuoka.q2@gmail.com>
2023-12-07 15:41:40 +0900

    feat: dont show api key component after input

--- src/pages/PrefecturePopulationPage.tsx
+++ src/pages/PrefecturePopulationPage.tsx
@@ -1,7 +1,9 @@
+import { useContext } from "react";
 import { GraphArea } from "../features/graph";
 import { PrefectureCheckList } from "../features/prefectures";
 import { usePrefecture } from "../hooks/usePrefecture";
 import { ApiKeyInputPage } from "./ApiKeyInputPage";
+import { ResasApiKeyContext } from "../provider/ResasApiKey";
 
 export const PrefecturePopulationPage = () => {
   // 初期化・State作成
@@ -12,22 +14,28 @@ export const PrefecturePopulationPage = () => {
     populationCompositionDict,
     setPopulationCompositionDict,
   } = usePrefecture();
+  const apiKey = useContext(ResasApiKeyContext);
 
   return (
     <div>
       <h1>都道府県人口グラフ</h1>
-      <ApiKeyInputPage />
-      <PrefectureCheckList
-        prefectureDict={prefectureDict}
-        setPrefectureIsChecked={setPrefectureIsChecked}
-        populationCompositionDict={populationCompositionDict}
-        setPopulationCompositionDict={setPopulationCompositionDict}
-      />
-      <GraphArea
-        prefectureIsChecked={prefectureIsChecked}
-        populationCompositionDict={populationCompositionDict}
-        prefectureDict={prefectureDict}
-      />
+      {apiKey ? (
+        <>
+          <PrefectureCheckList
+            prefectureDict={prefectureDict}
+            setPrefectureIsChecked={setPrefectureIsChecked}
+            populationCompositionDict={populationCompositionDict}
+            setPopulationCompositionDict={setPopulationCompositionDict}
+          />
+          <GraphArea
+            prefectureIsChecked={prefectureIsChecked}
+            populationCompositionDict={populationCompositionDict}
+            prefectureDict={prefectureDict}
+          />
+        </>
+      ) : (
+        <ApiKeyInputPage />
+      )}
     </div>
   );
 };
